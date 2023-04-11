c97d9d7406cc54e2da6117525b35f4f96c2ff4c9
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-11 15:41:06 +0300

    run application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Forecast from "./components/Forecast";
+import SearchInput from "./components/SearchInput";
+import Temperature from "./components/Temperature";
+import TimeAndLocation from "./components/TimeAndLocation";
+import getFormattedWeatherData from "./services/weatherService";
 
 function App() {
+  const [cityQuery, setCityQuery] = useState({ q: "antalya" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const weatherData = async () => {
+      await getFormattedWeatherData({ ...cityQuery, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    weatherData();
+  }, [cityQuery, units]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <SearchInput setCityQuery={setCityQuery} />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <Temperature weather={weather} />
+          <Forecast title="Saatlik Tahmin" items={weather.hourly} />
+          <Forecast title="Günlük Tahmin" items={weather.daily} />
+        </div>
+      )}
     </div>
   );
 }
