3def01403c86372dd909d34bcf8e2c29a14066ff
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-11 16:21:10 +0300

    context added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeContextProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </BrowserRouter>
 );
